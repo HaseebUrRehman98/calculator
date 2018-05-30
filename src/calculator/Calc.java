bfdf8a7a7c5231ace8bcb0bd849c8f96bcd77a0e
haseeb <haseeb@haseeb>
2018-05-30 18:42:13 +0200

    Calculator

--- src/calculator/Calc.java
+++ src/calculator/Calc.java
@@ -7,17 +7,30 @@ package calculator;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Label;
+import java.awt.LayoutManager;
+import java.awt.MouseInfo;
+import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.math.BigDecimal;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
+import javax.swing.plaf.basic.BasicBorders;
 
 /**
  *
@@ -25,91 +38,241 @@ import javax.swing.border.Border;
  */
 public class Calc extends javax.swing.JFrame {
 
-    private JButton b[][];
-    private String[] caratteri = {"<-", "CE", "C", "^", "1", "2", "3", "/", "4", "5", "6", "-", "7", "8", "9", "*", "0", ".", "+", "="};
-    private ActionListener listner;
+    private int prova1 = 0;
+    private int moveflag = 0;
+    private JButton buttons[][];
+    private String[] caratteri = {"<-", "CE", "C", "x²", "1", "2", "3", "/", "4", "5", "6", "-", "7", "8", "9", "*", "0", ".", "+", "="};
     private KeyListener key;
-    private JLabel label;
+    protected JLabel resultlabel;
     private int k = 0;
     private int l;
-    private int h = 50;
+    private int h = 85;
     private int operatorflag = 0;
     private int numberflag = 1;
     private Calculator calcolo;
+    private JButton X;
+    private JFrame jframe;
+    private JButton maximize;
+    private int lastX, lastY;
+    private MouseListener mouse;
+
+    private JLabel name;
+    private JPanel movepanel;
 
     public Calc() {
-        initComponents();
-        calcolo = new Calculator();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        this.setTitle("Calculator 1.0");
-        this.setSize(250, 300);
+        this.setUndecorated(true);
+        this.setSize(220, 300);
+        this.setVisible(true);
         this.setResizable(false);
-        // intialize
-        label = new JLabel();
-        b = new JButton[5][4];
+        //this.setLayout(null);
+        this.getRootPane().setBorder(BorderFactory.createBevelBorder(1, Color.GREEN, Color.GREEN));
+        this.setBackground(new Color(0, 0, 0, 1));
+
         key = new KeyListener() {
             @Override
             public void keyTyped(KeyEvent ke) {
+
             }
 
             @Override
             public void keyPressed(KeyEvent ke) {
-                //System.err.println(ke.getKeyCode());
-                for (int i = 0; i < b.length; i++) {
-                    for (int j = 0; j < b[i].length; j++) {
-                        if (b[i][j].getText().equals(ke.getKeyChar() + "")) {
-                           label.setText(calcolo.valueChecker(label.getText(), ke.getKeyChar() + ""));
+                for (int i = 0; i < buttons.length; i++) {
+                    for (int j = 0; j < buttons[i].length; j++) {
+                        if (buttons[i][j].getText().equals(ke.getKeyChar() + "")) {
+                            resultlabel.setText(calcolo.valueChecker(resultlabel.getText(), ke.getKeyChar() + ""));
+                            adjustTextSize();
                         }
+                    }
+                }
+                if (ke.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
+                    resultlabel.setText(calcolo.valueChecker(resultlabel.getText(), "<-" + ""));
+                }
+                if (ke.getKeyChar() == KeyEvent.VK_ENTER) {
+                    resultlabel.setText(calcolo.valueChecker(resultlabel.getText(), "=" + ""));
+                }
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent ke) {
+
+            }
+
+        };
+        mouse = new MouseListener() {
+            JButton button2 = null;
+
+            @Override
+            public void mouseClicked(MouseEvent me) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+
+                if (me.getComponent() instanceof JButton) {
+                    JButton button = (JButton) me.getComponent();
+                    if (button.getText().equals("X")) {
+                        System.exit(0);
+                    } else if (button.getText().equals("_")) {
+                        jframe.setState(Frame.ICONIFIED);
+                    } else {
+
+                        resultlabel.setText(calcolo.valueChecker(resultlabel.getText(), button.getText()));
+                        adjustTextSize();
+                    }
+                }
+                if (me.getComponent() instanceof JPanel) // check if mousepressed on Panel
+                {
+                    lastX = me.getXOnScreen();
+                    lastY = me.getYOnScreen();
+                    moveflag = 1;
+                }
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) { // moveJframe
+                if (moveflag == 1) {
+                    int x = me.getXOnScreen();
+                    int y = me.getYOnScreen();
+                    setLocation(getLocationOnScreen().x + x - lastX,
+                            getLocationOnScreen().y + y - lastY);
+                    lastX = x;
+                    lastY = y;
+                    moveflag = 0;
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) { //check if mouse enter mimize and X
+                if (me.getComponent() instanceof JButton) {
+                    JButton button = (JButton) me.getComponent();
+                    if (button.getText().equals("X") || button.getText().equals("_")) {
+                        button.setBackground(Color.green);
+                        button2 = button;
                     }
                 }
             }
 
             @Override
-            public void keyReleased(KeyEvent ke) {
-            }
+            public void mouseExited(MouseEvent me) { // colorchange
+                if (button2 != null) {
+                    button2.setBackground(new Color(0, 0, 0, 0));
+                }
+                button2 = null;
 
-        };
-
-        listner = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) 
-            {
-                label.setText(calcolo.valueChecker(label.getText(), ae.getActionCommand()));
             }
         };
-        // format setting jlabel and button
-        label.setText("0");
-        label.setBounds(12, 5, 175, 45);
-        label.setBackground(Color.BLACK);
-        label.setForeground(Color.white);
-        label.setOpaque(true);
-        label.setHorizontalAlignment(SwingConstants.RIGHT);
-        label.setBorder(BorderFactory.createLineBorder(Color.black));
-        this.add(label);
+
+        //initComponents();
+        calcolo = new Calculator();
+        movepanel = new JPanel();
+        X = new JButton();
+        maximize = new JButton();
+        name = new JLabel();
+        resultlabel = new JLabel();
+        buttons = new JButton[5][4];
+
+        // name 
+        name.setText("CALCULATOR 1.0");
+        name.setHorizontalAlignment(SwingConstants.CENTER);
+        name.setForeground(Color.GREEN);
+        name.setSize(120, 40);
+        name.setBackground(new Color(0, 0, 0, 1));
+        name.setFont(new Font("SansSerif", Font.ITALIC, 12));
+        //move panel
+        movepanel.setSize(220, 40);
+        movepanel.setName("movepanel");
+        movepanel.setBackground(Color.LIGHT_GRAY);
+        movepanel.addMouseListener(mouse);
+        movepanel.setBackground(new Color(0, 0, 0, 128));
+
+        //minmize setting
+        X.setText("_");
+        X.setName("minimize");
+        X.setBounds(137, 0, 40, 40);
+        X.setBackground(new Color(0, 0, 0, 0));
+        X.setHorizontalAlignment(SwingConstants.CENTER);
+        X.setBorder(BorderFactory.createLineBorder(Color.yellow));
+        X.setForeground(Color.black);
+        X.addMouseListener(mouse);
+        // maximize setting
+        maximize.setText("X");
+        maximize.setBounds(175, 0, 40, 40);
+        maximize.setBackground(new Color(0, 0, 0, 0));
+        maximize.setForeground(Color.black);
+        maximize.setHorizontalAlignment(SwingConstants.CENTER);
+        maximize.setBorder(BorderFactory.createLineBorder(Color.yellow));
+        maximize.addMouseListener(mouse);
+
+        // result show label
+        resultlabel.setText("0");
+        resultlabel.setBounds(12, 40, 178, 45);
+        resultlabel.setBackground(new Color(0, 0, 0, 200));
+        resultlabel.setForeground(Color.white);
+        resultlabel.setOpaque(true);
+        resultlabel.setAutoscrolls(true);
+        resultlabel.setBorder(BorderFactory.createLineBorder(Color.black));
+        resultlabel.setFont(new Font("Verdana", Font.ITALIC, 15));
+        resultlabel.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        // adding
+       
+        this.add(resultlabel);
+        this.add(X);
+        this.add(maximize);
+        this.add(name);
+        this.add(movepanel);
+
         int converter = 0;
-        for (int i = 0; i < b.length; i++) {
+
+        for (int i = 0; i < buttons.length; i++) {
             l = 10;
-            for (int j = 0; j < b[i].length; j++) {
-                b[i][j] = new JButton();
-                b[i][j].setText(caratteri[k]);
-                b[i][j].setBounds(l, h, 45, 40);
-                b[i][j].addActionListener(listner);
-                b[i][j].addKeyListener(key);
+            for (int j = 0; j < buttons[i].length; j++) {
+
+                buttons[i][j] = new JButton();
+                buttons[i][j].setText(caratteri[k]);
+
+                buttons[i][j].setBounds(l, h, 47, 40);
+                buttons[i][j].addKeyListener(key);
+                buttons[i][j].addMouseListener(mouse);
+
                 try {
                     converter = (int) Integer.parseInt(caratteri[k]); // adding different background to numbers button
-                    b[i][j].setBackground(Color.CYAN);
+                    buttons[i][j].setBackground(Color.CYAN);
+                    buttons[i][j].setForeground(Color.BLACK);
                 } catch (NumberFormatException io) {
-                    b[i][j].setBackground(Color.yellow);
-
+                    buttons[i][j].setBackground(Color.black);
+                    buttons[i][j].setForeground(Color.white);
                 } finally {
-                    this.add(b[i][j]);
+                    this.add(buttons[i][j]);
                     l += 45;
                     k++;
                 }
+
             }
             h += 40;
+            JButton prova=new JButton();
+           prova.setSize(1,1);
+           this.add(prova);
         }
+        // this.pack();
 
+    }
+
+    public void adjustTextSize() {
+        Font labelFont = resultlabel.getFont();
+        String labelText = resultlabel.getText();
+        int resultlabeltextWidth = resultlabel.getFontMetrics(labelFont).stringWidth(labelText);
+        int resultlabelWidth = resultlabel.getWidth();
+        if (resultlabeltextWidth >= resultlabelWidth - 20) {
+            resultlabel.setFont(new Font(labelFont.getName(), Font.PLAIN, resultlabel.getFont().getSize() - 1));
+        } else {
+            resultlabel.setFont(new Font("Verdana", Font.ITALIC, 15));
+        }
     }
 
     /**
@@ -124,6 +287,8 @@ public class Calc extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+        setFocusCycleRoot(true);
         getContentPane().setLayout(null);
         getContentPane().add(jLabel1);
         jLabel1.setBounds(440, 278, 230, 0);
@@ -159,6 +324,7 @@ public class Calc extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Calc().setVisible(true);
+
             }
         });
     }
@@ -166,4 +332,5 @@ public class Calc extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
+
 }
--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -5,6 +5,10 @@
  */
 package calculator;
 
+import com.sun.javafx.font.Metrics;
+import java.awt.Font;
+import static java.nio.file.Files.size;
+
 /**
  *
  * @author haseeb
@@ -14,157 +18,230 @@ public class Calculator {
     private int pointerCounter;
     private int operatorCounter = 0;
     private int numberCounter = 1;
+    private int numberCounter1=0;
     private String previousLabel;
-    private String sign="";
+    private String sign = "";
     private String labelValueArray[];
-    String segno = "";
+    private String segno = "";
     private int flag = 0;
+    private String Actioncommand = "";
+    private boolean equalFlag=false;
 
-    private final String CE(String a) {
-        pointerCounter = 0;
+    private final String CE(String a) 
+    {
+       pointerCounter = 0;
+       numberCounter=0;
         String b[] = a.split("");
-        int flag = 0;
-        a = "";
-        for (int i = 0; i < b.length; i++) {
-            if (isOperator(b[i]) == false) {
-                a = a + b[i];
-            } else {
-                a = a + b[i];
-                flag = 1;
-                break;
+       int flag=-1;
+        
+        for(int i=b.length-1;i>0;i--)
+        {
+            if(isOperator(b[i])==false);
+            else
+            {
+              flag=i;
+              break;
             }
         }
-        if (flag == 0) {
+        if (flag == -1) {
             return "0";
-        } else {
-            return a;
+        } 
+        else {
+            return a.substring(0, flag+1);
         }
     }
 
-    private final String backspace(String a) {
+    private final String backspace(String a) 
+    {
         String b[] = a.split("");
-        for (int i = b.length - 1; i >= 0; i--) {
-            if (isOperator(b[i]) == false) {
-                a = "";
-                for (int k = 0; k < b.length - 1; k++) {
-                    a = a + b[k];
-                }
-                if (b[i].equals(".")) {
-                    pointerCounter = 0;
-                }
-            } else {
-                numberCounter = 1;
-            }
-            break;
+        
+         if(isOperator(b[b.length-1]));
+         else
+         {
+              a=a.substring(0,a.length()-1);
+         }
+         if (a.length() == 0) {
+            return "0";
         }
-        if (a.length() == 0) {
-            a = "0";
-        }
-        return a;
-    }
+         else 
+         {
+            return a; 
+         }
+       }
 
-    public final String valueChecker(String label, String ActionComand) {
-        String labelArray[] = label.split("");
+    public final String valueChecker(String label, String ActionComand) 
+    {
+        if(equalFlag==true) // after equal checker
+        {
+           if(isOperator(ActionComand));
+            else label="0";
+            equalFlag=false;
+        }
+        if(label.equals("Infinity"))
+        {
+            label="0";
+        }
+        this.Actioncommand = ActionComand;
         if (ActionComand.equals("CE")) {
             return CE(label);
         } else if (ActionComand.equals("C")) {
             pointerCounter = 0;
             operatorCounter = 0;
             numberCounter = 1;
-            sign="";
+            sign = "";
             return "0";
-        } else if (ActionComand.equals("<-")) {
-            return backspace(label);
-        } else if (ActionComand.equals("=")) // precedenza
+        } else if (ActionComand.equals("<-")) 
         {
-            if (numberCounter > 1) {
-                numberCounter--;
-                operatorCounter--;
+            return backspace(label);
+        }
+        else if (ActionComand.equals("=")) // precedenza
+        {
+            if (numberCounter >=2 && operatorCounter>0) 
+            {
+                equalFlag=true;
                 return calcolo(label);
-            } else {
+            } 
+            else {
                 return label;
             }
-
         } 
         else if (ActionComand.equals(".")) {
-            if (isOperator(labelArray[labelArray.length - 1]) || pointerCounter > 0) {
+            if (isOperator(label.charAt(label.length() - 1) + "") || pointerCounter > 0) {
                 return label;
             } else {
                 pointerCounter++;
                 return label + ActionComand;
             }
-        } else if (label.equals("0") && isOperator(ActionComand) == false) 
+        } else if (isOperator(ActionComand)) // operatori
         {
-            return ActionComand;
-        } else if (labelArray[labelArray.length - 1].equals(".") && isOperator(ActionComand)) {
-            return label;
-        } else if (ActionComand.equals("^")) 
+            numberCounter1=0;
+            return operatorChecker(label, ActionComand);
+        } 
+        else  if(numberCounter1<13) // numeri
         {
-            if (isOperator(label.split("")[label.length() - 1])) {
-                operatorCounter--;
-                return afterPoint(Double.parseDouble(labelArray[0]) * Double.parseDouble(labelArray[0]) + "");
+            numberCounter1++;
+            if (label.equals("0")) 
+            {
+                return ActionComand;
+            } 
+            else {
+                numberCounter++;
+                return label + ActionComand;
             }
-            if (operatorCounter > 0) {
-                segno = operatorSearch(label);
-                labelValueArray = label.split("\\" + segno);
-                return labelValueArray[0] + segno + afterPoint(Double.parseDouble(labelValueArray[1]) * Double.parseDouble(labelValueArray[1]) + ""); // operator divide 
+        }
+        else return label;
+    }
+
+    private String operatorChecker(String label, String ActionComand) 
+    {
+        pointerCounter = 0; 
+        previousLabel = "";
+        String labelArray[] = label.split("");
+        if (ActionComand.equals("x²")) 
+        {
+            if (isOperator(label.charAt(label.length() - 1) + "") || (label.charAt(label.length() - 1) + "").equals(".")) {
+                return label;
             } else {
-                return afterPoint(Double.parseDouble(label) * Double.parseDouble(label) + "");
+                label = "";
+                for (String i: labelArray) 
+                {
+                    if (isOperator(i) == false) {
+                        label = label + i;
+                    } else {
+                        previousLabel = previousLabel + label + i;
+                        label = "";
+                    }
+                }
+                return previousLabel + afterPoint(Double.parseDouble(label) * Double.parseDouble(label) + "");
             }
-        } else if (isOperator(labelArray[labelArray.length - 1]) && isOperator(ActionComand)) 
+        } 
+        else if ((label.charAt(label.length() - 1) + "").equals(".")  && isOperator(ActionComand)) //  !!.+!!
         {
-            label = "";
-            for (int i = 0; i < labelArray.length - 1; i++) {
-                label = label + labelArray[i];
-            }
-            label = label + ActionComand;
-            sign=ActionComand;
             return label;
         } 
-        else 
+        
+        else if (isOperator(label.charAt(label.length() - 1) + "") && isOperator(ActionComand)) // change operator 
         {
-
-            if (isOperator(ActionComand) && ActionComand != "^") 
+         
+            label = "";
+          for (int i = 0; i < labelArray.length - 1; i++) 
+          {
+                label = label + labelArray[i];
+          }
+            if (ActionComand.equals("-") && numberCounter > 1 || ActionComand.equals("+") && numberCounter > 1 || operatorCounter > 1) 
             {
-                if(operatorCounter<1 || sign.equals(""))
-                {
-                    sign=ActionComand;
-                   
-                }
-                operatorCounter++;
-                flag = 1;
-                pointerCounter = 0;
-                if (operatorCounter > 1) 
-            {
-                if(ActionComand.equals(sign) || ActionComand.equals("-") ||ActionComand.equals("+"))
-                {
-                  label = calcolo(label);
-                  sign=ActionComand;
-                  operatorCounter--;
-                  numberCounter--;
-                }
-              else
-                {
-                  sign=ActionComand;
-                  return label+ActionComand;   
-                }
+                label = calcolo(label);
             }
-            } 
-            else if (flag != 0) 
+            sign = ActionComand;
+            label = label + ActionComand;
+            return label;
+        } 
+        else {
+            operatorCounter++;
+            if (operatorCounter >= 2 && priority(sign, ActionComand) == 0 && numberCounter>1) 
             {
-                numberCounter++;
-                flag = 0;
+                sign = ActionComand;
+                label = calcolo(label);
+                label += ActionComand;
             }
-            
+            else 
+            {
+             
+                numberCounter1=0;
+                sign = ActionComand;
+                label = label + ActionComand;
+            }
         }
-        return label + ActionComand;
+
+        return label;
     }
 
     private final String calcolo(String a) {
-        String result = "error";
-        segno = operatorSearch(a);
-        labelValueArray = a.split("\\" + segno);
-        if (segno.equals("/")) {
+        String numero = "";
+        String segno1 = "";
+        String result = "error1";
+        if (operatorCounter > 2 || Actioncommand.equals("=") && operatorCounter==2 ) 
+        {
+            numberCounter -= 2;
+            operatorCounter--;
+            segno1 = operatorSearch(a);
+            String prova[] = a.split("\\" + segno1);
+            numero = prova[0];
+            a = prova[1];
+            segno = operatorSearch(a);
+            if (isOperator(a.charAt(0) + "") && segno.equals("-")) 
+            {
+                labelValueArray = a.substring(1, a.length()).split(segno);
+                labelValueArray[0] = a.charAt(0) + labelValueArray[0];
+            } else 
+            {
+                labelValueArray = a.split("\\" + segno);
+            }
+        } 
+        else {
+           
+            if (Actioncommand.equals("=")) {
+                operatorCounter = 0;
+            } 
+            else 
+            {
+                operatorCounter--;
+            }
+            segno = operatorSearch(a);
+            if (isOperator(a.charAt(0) + "") && segno.equals("-")) 
+            {
+                labelValueArray = a.substring(1, a.length()).split(segno);
+                labelValueArray[0] = a.charAt(0) + labelValueArray[0];
+            } 
+            else 
+            {
+                labelValueArray = a.split("\\" + segno);
+            }
+            numberCounter=1;
+        }
+
+        if (segno.equals("/")) 
+        {
             result = (Double.parseDouble(labelValueArray[0]) / Double.parseDouble(labelValueArray[1])) + "";
         }
         if (segno.equals("+")) {
@@ -176,30 +253,47 @@ public class Calculator {
         if (segno.equals("*")) {
             result = (Double.parseDouble(labelValueArray[0]) * Double.parseDouble(labelValueArray[1])) + "";
         }
+        if (result.equals("error1")); 
+        else {
+            if (Actioncommand.equals("=") && numero != "" || Actioncommand.equals("+") && numero != "" || Actioncommand.equals("-") && numero != "") {
+                return result = afterPoint(calcolo(numero + segno1 + result));
 
-        if (result.equals("error")); else {
-            return afterPoint(result);
+            } else {
+                return result = numero + segno1 + afterPoint(result);
+            }
         }
-        return "error";
+
+        return result;
     }
 
     private String afterPoint(String result) // calculate values after point
     {
+        
         String[] resultArray = result.split("");
+       
         flag = 0;
+        int count2=0;
         int count = 0;
-        for (int i = 0; i < resultArray.length; i++) {
-            if (flag == 1) {
+        for (String i :  resultArray) 
+        {
+            if (flag == 1) 
+            {
                 count++;
             }
-            if (resultArray[i].equals(".")) {
+            if (i.equals(".")) 
+            {
                 flag = 1;
             }
+            
         }
-        if (count > 1) {
+        if (count > 1) 
+        {
             return result;
-        } else {
-            if (result.split("")[result.length() - 1].equals("0")) {
+        } 
+        else 
+        {
+            if (result.split("")[result.length() - 1].equals("0")) 
+            {
                 return (long) Double.parseDouble(result) + "";
             }
             return result;
@@ -207,7 +301,7 @@ public class Calculator {
     }
 
     private final boolean isOperator(String c) {
-        if (c.equals("+") || c.equals("^") || c.equals("-") || c.equals("*") || c.equals("/")) {
+        if (c.equals("+") || c.equals("x²") || c.equals("-") || c.equals("*") || c.equals("/")) {
             return true;
         }
 
@@ -216,13 +310,40 @@ public class Calculator {
 
     private final String operatorSearch(String a) {
         labelValueArray = a.split("");
-        for (int i = 0; i < labelValueArray.length; i++) {
-            if (isOperator(labelValueArray[i]) == false); else {
-                a = labelValueArray[i];
-                break;
+        for (int i=1;i<labelValueArray.length;i++) 
+        {
+            if (isOperator(labelValueArray[i]) == false);
+            else 
+            {
+                return labelValueArray[i];
             }
         }
-        return a;
+        return "error";
     }
 
+    private int priority(String sign, String ActionCommand) 
+    {
+        if (sign.equals(ActionCommand)) 
+        {
+            return 0;
+        }
+        else if (sign != ActionCommand && ActionCommand.equals("+") || ActionCommand.equals("-")) 
+        {
+            return 0;
+        }
+        else if ((sign.equals("/") || sign.equals("*"))  && (ActionCommand.equals("*") || ActionCommand.equals("/"))) // errore
+        {
+            return 0;
+        } 
+        else 
+        {
+            return 1;
+        }
+    }
+
+    // migliorare grafica
+    
+    // CE problem operator
+    
+    
 }
